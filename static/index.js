8eccfe420f7a352234c21d42a4e98195fb2305ea
xiaot2333 <98317530+xiaot2333@users.noreply.github.com>
2022-01-24 19:08:49 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return '您是gachi？';
+    if (score <= 99) return 'bbzl';
+    if (score <= 149) return '我不好说了';
+    if (score <= 199) return '您就是anti头子？';
     return '人？';
 }
 
